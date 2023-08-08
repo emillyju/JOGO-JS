841a12658280f40f3ba03aed2672acf0c794b5ff
emillyju <140511455+emillyju@users.noreply.github.com>
2023-08-08 09:18:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+  background("rgb(27,243,4)");
+}
+
+function draw() {
+  
+  
+  //console.log(mouseIsPressed);
+  
+  if(mouseIsPressed){
+   rect(mouseX,mouseY, 15, 15);
+  }
+  
+ fill("rgb(25,25,25)");
+  stroke("white")
+
+  
+  
+}
